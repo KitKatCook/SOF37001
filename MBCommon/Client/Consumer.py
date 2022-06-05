93564edc93fe8cdaf708a125850ca20524888141
KitKatCook <kitcookdev12@outlook.com>
2022-06-05 23:32:32 +0100

    add

--- MBCommon/Client/Consumer.py
+++ MBCommon/Client/Consumer.py
@@ -1,6 +1,10 @@
+from wsgiref.validate import InputWrapper
 from Client import Client
 
 class Consumer(Client):
       def __init__(self, args):
             Client.__init__(self, args)
             print("I am a Consumer")
+
+      def sendMessage(self, message):
+            return message
