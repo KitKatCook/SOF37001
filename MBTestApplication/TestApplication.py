983b2f94e9646fac4098f076f8ecc52e13f21285
KitKatCook <kitcookdev12@outlook.com>
2022-06-12 00:18:36 +0100

    update test Application

--- MBTestApplication/TestApplication.py
+++ MBTestApplication/TestApplication.py
@@ -6,13 +6,22 @@ window = tk.Tk()
 window.minsize(720,480)
 window.title("Message Broker Test Application")
 
-def setTextInput(text):
+def addMessageLogText(text):
     logTabOutput.insert(1.0, text + "\n" )
+
+def addErrorLogText(text):
+    errorTabOutput.insert(1.0, text + "\n" )
+
+def setResultsTime(text):
+    errorTabOutput.insert(1.0, text + "\n" )
+
+def setResultsMessages(text):
+    errorTabOutput.insert(1.0, text + "\n" )
 
 greeting = tk.Label(text="Welcome")
 greeting.pack()
 
-runButton = tk.Button(window, text = 'Run Test!', command=lambda:setTextInput("new content")).pack()
+runButton = tk.Button(window, text = 'Run Test!', command=lambda:addMessageLogText("new content")).pack()
 
 tabparent = Notebook(window)
 
@@ -24,23 +33,31 @@ tabparent.add(errorTab, text="Errors")
 
 tabparent.pack(expand=1, fill='both')
 
-logTabOutput = Text(logTab, height = 30,
-              width = 25)
+
+logTabOutputScrollBar = tk.Scrollbar(logTab, orient='vertical')
+errorTabOutputScrollBar = tk.Scrollbar(errorTab, orient='vertical')
+
+
+logTabOutput = Text(logTab, height = 30, width = 25, yscrollcommand=logTabOutputScrollBar.set)
 logTabOutput.pack(fill='both', expand=True, side=tk.LEFT)
 logTabOutput.insert(1.0, "No Messages.")
 
-errorTabOutput = Text(errorTab, height = 30,
-              width = 25)
+
+errorTabOutput = Text(errorTab, height = 30, width = 25, yscrollcommand=errorTabOutputScrollBar.set)
 errorTabOutput.pack(fill='both', expand=True, side=tk.LEFT)
 errorTabOutput.insert(1.0, "No Errors.")
 
-logTabOutputScrollBar = tk.Scrollbar(logTab, orient='vertical')
+
 logTabOutputScrollBar.pack(side=tk.RIGHT, fill='y')
 logTabOutputScrollBar.config(command=logTabOutput.yview)
 
-errorTabOutputScrollBar = tk.Scrollbar(errorTab, orient='vertical')
+
 errorTabOutputScrollBar.pack(side=tk.RIGHT, fill='y')
 errorTabOutputScrollBar.config(command=errorTabOutput.yview)
+
+
+
+timeResults = tk.Entry(window, )
 
 
 window.mainloop()
